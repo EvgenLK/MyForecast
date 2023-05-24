a932dabb706eb4aeb49520a8150329e0fe05a412
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2023-05-24 22:37:56 +1000

    add image weather

--- MyForecast/ViewController.swift
+++ MyForecast/ViewController.swift
@@ -9,20 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     let backGroundImage = UIImageView(frame: UIScreen.main.bounds)
-    var myLabelTemperature = UILabel()
+    var myCurrentTemp = UILabel()
+    var myCurrentPrecipitation = UILabel()
+    var myCurrentWind = UILabel()
     var myTextFieldTemperature = UITextField()
+    var myCurrentDate = UILabel()
+    var myIconWeather: [String] = []
+
     var myButtonTemperature = UIButton()
     var forecastWeatherDay: [String] = []
     let queue = DispatchQueue(label: "thred-save-inArray", attributes: .concurrent)
+    let queueImage = DispatchQueue(label: "thred-save-image", attributes: .concurrent)
     var arrayCityForecast: [String] = []
     var forecastRain: [String] = []
     var forecastWindSpeed: [String] = []
     var forecastDate: [String] = []
-    var currentData: Int = {
+    var currentHour: Int = {
         let date = Date()
         let calendar = Calendar.current
         let hour = calendar.component(.hour, from: date)
         return hour
+    }()
+    var currentDateforLabel: String = {
+       
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+
+
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        let updDate = formatter.string(from: currentDateTime)
+        
+        return updDate
     }()
     
     
@@ -35,12 +53,10 @@ class ViewController: UIViewController {
         setupLabel()
         setupButtonTemp()
         self.title = "My Forest Weather"
+
+        
         setupTextField()
         colView()
-    }
-    
-    func setupScrollView() {
-        
     }
     
     
@@ -63,11 +79,11 @@ class ViewController: UIViewController {
     @objc func requestForecastWeather() {
         myTextFieldTemperature.resignFirstResponder()
         if myTextFieldTemperature.text!.replacingOccurrences(of: " ", with: "") != ""{
-            myLabelTemperature.text = "Loading..."
+            myCurrentTemp.text = "Loading..."
             
             let urlCityString = "https://geocoding-api.open-meteo.com/v1/search?name=\(myTextFieldTemperature.text!.replacingOccurrences(of: " ", with: ""))&count=1&language=en&format=json"
             
-            guard let urlcity = URL(string: urlCityString) else {fatalError("fail")}
+            guard let urlcity = URL(string: urlCityString) else { return }
             let requestCity = URLRequest(url: urlcity)
             
             let taskCity = URLSession.shared.dataTask(with: requestCity) { data, response, error in
@@ -92,7 +108,7 @@ class ViewController: UIViewController {
                 }
             }
             taskCity.resume()
-            sleep(2)
+            sleep(3)
             
             
             let urlString = "https://api.open-meteo.com/v1/gfs?latitude=\(self.arrayCityForecast[0])&longitude=\(self.arrayCityForecast[1])&hourly=temperature_2m,precipitation,windspeed_10m&windspeed_unit=ms&forecast_days=1&timezone=auto"
@@ -105,45 +121,77 @@ class ViewController: UIViewController {
                     self.forecastWeatherDay.removeAll()
                     
                     self.queue.async(flags: .barrier){ [weak self] in
+                        //
                         for elem in 0..<24{
-                            guard let temp = weather.hourly?.temperature2M![elem] else {return}
-                            self?.forecastWeatherDay.append(String(Int(temp)))
+                            if elem % 3 == 0{
+                                guard let temp = weather.hourly?.temperature2M![elem] else {return}
+                                self?.forecastWeatherDay.append(String(Int(temp)))
+                            }
                         }
-                    }
+
+                        for elem in 0..<24{
+                            if elem % 3 == 0{
+                                guard let rain = weather.hourly?.precipitation![elem] else {return}
+                                self?.forecastRain.append(String(Double(rain)))
+                            }
+                        }
                     
-                    self.queue.async(flags: .barrier){ [weak self] in
                         for elem in 0..<24{
-                            guard let rain = weather.hourly?.precipitation![elem] else {return}
-                            self?.forecastRain.append(String(Double(rain)))
+                            if elem % 3 == 0{
+                                guard let windSpeed = weather.hourly?.windspeed10M![elem] else { return }
+                                self?.forecastWindSpeed.append(String(Double(windSpeed)))
+                            }
                         }
-                    }
-                    self.queue.async(flags: .barrier){ [weak self] in
+
                         for elem in 0..<24{
-                            guard let windSpeed = weather.hourly?.windspeed10M![elem] else { return }
-                            self?.forecastWindSpeed.append(String(Double(windSpeed)))
+                            if elem % 3 == 0{
+                                guard let StringDate = weather.hourly?.time![elem] else { return }
+                                
+                                let dateString = StringDate + ":00Z"
+                                let dateFormatter = ISO8601DateFormatter()
+                                let date = dateFormatter.date(from: dateString)
+                                
+                                let dateFormatter2 = ISO8601DateFormatter()
+                                dateFormatter2.formatOptions = .withFullTime
+                                var strDate = dateFormatter2.string(from: date!)
+                                strDate.removeLast(4)
+                                
+                                self?.forecastDate.append(strDate)
+                            }
                         }
-                    }
-                    self.queue.async(flags: .barrier){ [weak self] in
-                        for elem in 0..<24{
-                            guard let StringDate = weather.hourly?.time![elem] else { return }
+                        
+
                             
-                            let dateString = StringDate + ":00Z"
-                            let dateFormatter = ISO8601DateFormatter()
-                            let date = dateFormatter.date(from: dateString)
-                            
-                            let dateFormatter2 = ISO8601DateFormatter()
-                            dateFormatter2.formatOptions = .withFullTime
-                            var strDate = dateFormatter2.string(from: date!)
-                            strDate.removeLast(4)
-                            
-                            self?.forecastDate.append(strDate)
-                        }
+                            for elem in 0..<24{
+                                if elem % 3 == 0{
+                                    guard let precipitation = weather.hourly?.precipitation![elem] else { return }
+                                    
+                                    switch elem {
+                                    case 0,1,2,3,4,5,20,21,22,23,24:
+                                        if precipitation == 0{
+                                            self?.myIconWeather.append("clearNight")
+                                        } else if precipitation > 1.0 {
+                                            self?.myIconWeather.append("rainNight")
+                                        }
+                                    default:
+                                        self?.myIconWeather.append("sun")
+                                        
+                                    }
+                                }
+                            }
+                        
                         self?.updateArrAsync()
                     }
                     
                     DispatchQueue.main.async { [weak self] in
-                        guard let temp = weather.hourly?.temperature2M![self!.currentData] else {return}
-                        self?.myLabelTemperature.text = "\(Int(temp))" + "℃"
+                        guard let temp = weather.hourly?.temperature2M![self!.currentHour] else {return}
+                        guard let precipitation = weather.hourly?.precipitation![self!.currentHour] else {return}
+                        guard let windSpeed = weather.hourly?.windspeed10M![self!.currentHour] else {return}
+
+                        self?.myCurrentTemp.text = "\(Int(temp))" + "℃"
+                        self?.myCurrentPrecipitation.text = "\(String(precipitation)) mm"
+                        self?.myCurrentWind.text = "\(String(windSpeed)) m/s"
+                        self?.myCurrentDate.text = self?.currentDateforLabel
                         
                         
                     }
@@ -165,6 +213,8 @@ class ViewController: UIViewController {
             _ = self.forecastRain
             _ = self.forecastWindSpeed
             _ = self.forecastDate
+            _ = self.myIconWeather
+            print(self.myIconWeather)
             self.collectionView.reloadData()
         }
     }
@@ -178,14 +228,39 @@ class ViewController: UIViewController {
 
         func setupLabel() {
             
-            myLabelTemperature = UILabel(frame: CGRect(x: 10, y: 170, width: view.bounds.width, height: 100))
-            myLabelTemperature.font = UIFont(name: "ArialMT", size: 50)
-            myLabelTemperature.textColor = .white
-            myLabelTemperature.text = "Enter the city"
-            myLabelTemperature.numberOfLines = 0
-            myLabelTemperature.textAlignment = .center
+            myCurrentTemp = UILabel(frame: CGRect(x: 10, y: 180, width: view.bounds.width, height: 100))
+            myCurrentTemp.font = UIFont(name: "ArialMT", size: 50)
+            myCurrentTemp.textColor = .white
+            myCurrentTemp.text = "Enter the city"
+            myCurrentTemp.numberOfLines = 0
+            myCurrentTemp.textAlignment = .center
             
-            self.view.addSubview(myLabelTemperature)
+            myCurrentPrecipitation = UILabel(frame: CGRect(x: 10, y: 330, width: view.bounds.width, height: 100))
+            myCurrentPrecipitation.font = UIFont(name: "ArialMT", size: 50)
+            myCurrentPrecipitation.textColor = .white
+            myCurrentPrecipitation.numberOfLines = 0
+            myCurrentPrecipitation.textAlignment = .center
+            
+            myCurrentWind = UILabel(frame: CGRect(x: 10, y: 380, width: view.bounds.width, height: 100))
+            myCurrentWind.font = UIFont(name: "ArialMT", size: 50)
+            myCurrentWind.textColor = .white
+            myCurrentWind.numberOfLines = 0
+            myCurrentWind.textAlignment = .center
+            
+            myCurrentDate = UILabel(frame: CGRect(x: 10, y: 150, width: view.bounds.width, height: 40))
+            myCurrentDate.font = UIFont(name: "ArialMT", size: 40)
+            myCurrentDate.textColor = .white
+            myCurrentDate.numberOfLines = 0
+            myCurrentDate.textAlignment = .center
+
+
+            
+            self.view.addSubview(myCurrentTemp)
+            self.view.addSubview(myCurrentPrecipitation)
+            self.view.addSubview(myCurrentWind)
+            self.view.addSubview(myCurrentDate)
+
+
         }
         
         func setupTextField() {
@@ -204,13 +279,18 @@ class ViewController: UIViewController {
         
         func setupBackGround() {
             
-            switch(currentData) {
+            switch(currentHour) {
             case 0,1,2,3,4,5,20,21,22,23,24:
+                
                 backGroundImage.image = UIImage(named: "Night")
                 backGroundImage.contentMode = .scaleAspectFill
+                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+
             default:
+                
                 backGroundImage.image = UIImage(named: "GoodDay")
                 backGroundImage.contentMode = .scaleAspectFill
+                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
             }
             view.addSubview(backGroundImage)
 
@@ -244,7 +324,6 @@ class ViewController: UIViewController {
         cv.layer.opacity = 0.6
         cv.layer.cornerRadius = 20
         cv.showsHorizontalScrollIndicator = false
-        
         return cv
 
     }()
@@ -268,10 +347,11 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDa
         
         if forecastWeatherDay.count != 0{
             
-            cell.myHour.text = "\(forecastDate[indexPath.row])"
+            cell.myHour.text = "\(forecastDate[indexPath.row]) hour"
             cell.myTemp.text = "\(forecastWeatherDay[indexPath.row]) ℃"
             cell.myRain.text = "\(forecastRain[indexPath.row])  mm"
             cell.myWindSpeed.text = "\(forecastWindSpeed[indexPath.row])  m/s"
+            cell.myImage.image = UIImage(named: myIconWeather[indexPath.row])
         } else {
             cell.myTemp.text = "Not forecast"
         }
