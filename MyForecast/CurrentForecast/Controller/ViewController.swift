528df6739a6ef5c7da391b3b0b9154d1d7dbe505
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2023-06-24 22:12:42 +1000

    create request weater data

--- MyForecast/CurrentForecast/Controller/ViewController.swift
+++ MyForecast/CurrentForecast/Controller/ViewController.swift
@@ -15,12 +15,14 @@ class ViewController: UIViewController, ButtonDelegate {
     var weather = [ModelDataWeather]()
     var lanlon = [WeatherCity]()
     var cityUtf8: String = ""
-    var weatherData =  [WeatherData]()
+    var weatherUnit =  [HourlyUnits]()
+    var weatherHourly =  [Hourly]()
+
 
     override func loadView() {
         view = customViewCurent
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "My Forest Weather"
@@ -32,65 +34,18 @@ class ViewController: UIViewController, ButtonDelegate {
         
         LanLonCityNetworkService.getLanLon(city: cityUtf8, day: day) { response in
             self.lanlon = response.lanlon
+            
+            WeatherNetworkService.getLanLon(latitude: self.lanlon[0].latitude, longitude: self.lanlon[0].longitude, days: day ) { response in
+                self.weatherUnit = response.weatherUnits
+                self.weatherHourly = response.weatherHourly
+                print(self.weatherUnit)
+                print(self.weatherHourly)
+                
+            }
         }
-        sleep(3)
-        print(self.lanlon)
-        WeatherNetworkService.getLanLon(latitude: lanlon[0].latitude, longitude: lanlon[0].longitude, days: day ){ response in
-            self.weatherData = response.weatherData
-        }
-        
     }
     
-// "https://api.open-meteo.com/v1/gfs?latitude=\(self.arrayCityForecast[0])&longitude=\(self.arrayCityForecast[1])&hourly=temperature_2m,precipitation,windspeed_10m&windspeed_unit=ms&forecast_days=\(forecastDaysWeather)&timezone=auto"
-    //
-    //                guard let url = URL(string: urlString) else { return }
-    //                let request = URLRequest(url: url)
-    //
-    //                let task = URLSession.shared.dataTask(with: request) { data, response, error in
-    //                    if let data, let weather = try? JSONDecoder().decode(WeaterData.self, from: data) {
-    //
-    //                        self.queue.async(flags: .barrier){ [weak self] in
-    //                            self!.dataForecastWeather.removeAll()
-    //
-    //                            for elem in 0..<24 * Int(forecastDaysWeather)!{
-    //                                if elem % 4 == 0{
-    //                                    guard let temp = weather.hourly?.temperature2M![elem] else { return }
-    //                                    guard let precipitation = weather.hourly?.precipitation![elem] else { return }
-    //                                    guard let windSpeed = weather.hourly?.windspeed10M![elem] else { return }
-    //                                    guard let StringDate = weather.hourly?.time![elem] else { return }
-    //
-    //
-    //                                    let dayForecast = modelForecast(date: modelDatFormat(StringDate: StringDate),
-    //                                                                    temp: String(Int(temp)),
-    //                                                                    windSpeed: String(Double(windSpeed)),
-    //                                                                    precipitation: String(Double(precipitation)),
-    //                                                                    imageIcon: modelimageIcon(precipitation: Int(precipitation), elem: elem))
-    //                                    self?.dataForecastWeather.append(dayForecast)
-    //                                }
-    //                            }
-    //
-    //                            self?.updateArrAsync()
-    //
-    //                        }
-    //
-    //                        DispatchQueue.main.async { [weak self] in
-    //                            guard let temp = weather.hourly?.temperature2M![self!.currentHour] else {return}
-    //                            guard let precipitation = weather.hourly?.precipitation![self!.currentHour] else {return}
-    //                            guard let windSpeed = weather.hourly?.windspeed10M![self!.currentHour] else {return}
-    //
-    //                            self?.myCurrentTemp.text = "\(Int(temp))" + "℃"
-    //                            self?.myCurrentPrecipitation.text = "\(String(precipitation)) mm"
-    //                            self?.myCurrentWind.text = "\(String(windSpeed)) m/s"
-    //                            self?.myCurrentDate.text = self?.currentDateforLabel
-    //
-    //
-    //                        }
-    //                    } else {
-    //                        print("Fail")
-    //                    }
-    //                }
-    //                task.resume()
-    //            }else {
+
     //                let alert = UIAlertController(title: "", message: "alert_not_city_request".localized, preferredStyle: UIAlertController.Style.alert)
     //                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
     //                self.present(alert, animated: true, completion: nil)
--- MyForecast/CurrentForecast/Model/WeatherData.swift
+++ MyForecast/CurrentForecast/Model/WeatherData.swift
@@ -5,54 +5,21 @@
 
 import Foundation
 
-// MARK: - WeatherData
-struct WeatherData: Codable {
-    let latitude, longitude, generationtimeMS: Double
-    let utcOffsetSeconds: Int
-    let timezone, timezoneAbbreviation: String
-    let elevation: Int
-    let hourlyUnits: HourlyUnits
-    let hourly: Hourly
-
-    enum CodingKeys: String, CodingKey {
-        case latitude, longitude
-        case generationtimeMS = "generationtime_ms"
-        case utcOffsetSeconds = "utc_offset_seconds"
-        case timezone
-        case timezoneAbbreviation = "timezone_abbreviation"
-        case elevation
-        case hourlyUnits = "hourly_units"
-        case hourly
-    }
-}
 
 // MARK: - Hourly
-extension WeatherData {
-    struct Hourly: Codable {
+
+    struct Hourly {
         let time: [String]
         let temperature2M: [Double]
-        let precipitation: [Int]
+        let precipitation: [Double]
         let windspeed10M: [Double]
-        
-        enum CodingKeys: String, CodingKey {
-            case time
-            case temperature2M = "temperature_2m"
-            case precipitation
-            case windspeed10M = "windspeed_10m"
-        }
+
     }
-}
 
 // MARK: - HourlyUnits
-extension WeatherData {
-    struct HourlyUnits: Codable {
+
+    struct HourlyUnits {
         let time, temperature2M, precipitation, windspeed10M: String
-        
-        enum CodingKeys: String, CodingKey {
-            case time
-            case temperature2M = "temperature_2m"
-            case precipitation
-            case windspeed10M = "windspeed_10m"
-        }
+
     }
-}
+
